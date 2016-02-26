de2bdcd34341bfb9e86034a74517436f8840f2fb
maratkenzhebulatov <marat.kenzhebulatov@academmedia.com>
2016-02-27 01:47:43 +0600

    add get post method

--- public/index.php
+++ public/index.php
@@ -21,11 +21,22 @@ $app->get('/getList', function (ServerRequestInterface $request, ResponseInterfa
     if (isset($id)) {
         $sql .= " AND id < $id ";
     }
-    $sql .= " ORDER BY id DESC LIMIT 0, $count";
+    $sql .= " ORDER BY id DESC LIMIT 0, $count ";
     $res = mysqli_query($db, $sql) or die($sql);
+    $items = [];
     while ($row = mysqli_fetch_object($res)) {
-        echo $row->id . " " . $row->description . "<br>";
+        $item  = [];
+        $item['id'] = $row->id;
+        $item['description'] = $row->description;
+        $baseUrl = "/img/" . $row->placeId . "/" . $row->id;
+        $item['expectationUrl'] = $baseUrl . "/exp.jpg";
+        $item['realityUrl'] = $baseUrl . "/real.jpg";
+        $items[] = $item;
     }
+    //print_r($items);
+    $response->getBody()->write(json_encode($items, JSON_UNESCAPED_SLASHES) );
+    return $response;
+//    echo  json_encode($items);
 });
 //$app->post('/postPhoto/[]')
 $app->run();
