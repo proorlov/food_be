f93b8e7fc805d9da62cc69a77e45a1f90b32a5dc
eagle <code@proorlov.ru>
2016-02-27 16:36:20 +0600

    headers

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
 require '../vendor/autoload.php';
 require 'connect.php';
 
